f70145c35e59ad7cba50b24a776dffa30d2a16a3
JanmejayaSwainOfficial <31533983+JanmejayaSwainOfficial@users.noreply.github.com>
2021-10-11 22:53:00 +0530

    Update mul.py

--- mul.py
+++ mul.py
@@ -1,1 +1,7 @@
 
+num_1 = input("Enter the first number pls")
+num_2 = input("Enter the second number pls")
+
+product = float(num_1) * float(num_2) 
+
+print("Product of {} and {} is {}".format(num_1, num_2,product))
